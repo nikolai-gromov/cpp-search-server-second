c09ee4e746853c77d380c3a5ee0256087f5a56a4
Nikolai <nikolay.gromov.u@yandex.ru>
2022-06-26 14:42:49 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikolai/projects/cpp-search-server-second
+# Build directory: /home/nikolai/projects/cpp-search-server-second/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
